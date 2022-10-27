03deaf8cb8f081c4ed4a774afec0afd6b6054377
Jay Jagpal <jay@strangemother.com>
2022-10-27 02:26:03 +0100

    filereadmode change

--- django-trim/trim/templatetags/link.py
+++ django-trim/trim/templatetags/link.py
@@ -26,6 +26,34 @@ def new_link(link, *targs, **kwargs):
     return gen_link(link, *targs, **kwargs)
 
 
+@register.inclusion_tag('trim/css_link.html', name='js', takes_context=False)
+def script_link(link, *targs, **kwargs):
+    kwargs.setdefault('type', 'text/javascript')
+    return {
+        'static_name': link,
+        'kwargs': kwargs,
+    }
+
+@register.inclusion_tag('trim/css_link.html', name='css', takes_context=False)
+def css_link(link, *targs, **kwargs):
+    """
+
+    Build a standard <link> for CSS, using a static path
+
+        {% css 'css/main.css' %}
+
+    All attributes of the node are available. As an example of versatility
+    (not recommended):
+
+        {% css 'images/favicon.ico' rel="shortcut icon" type="image/png" %}
+    """
+    kwargs.setdefault('rel', 'stylesheet')
+    kwargs.setdefault('type', 'text/css')
+    return {
+        'static_name': link,
+        'kwargs': kwargs,
+    }
+
 
 @register.inclusion_tag('trim/link.html', takes_context=False)
 def new_url_link(link, *targs, **kwargs):
