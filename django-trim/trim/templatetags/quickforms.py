8cd2dbf5ba926cbc5d863f46c99c220cefe20b9d
Jay Jagpal <jay@strangemother.com>
2022-11-01 02:11:51 +0000

    Forms and strings and live models

--- django-trim/trim/templatetags/quickforms.py
+++ django-trim/trim/templatetags/quickforms.py
@@ -7,7 +7,16 @@ register = template.Library()
 
 @register.simple_tag(takes_context=True, name='quickform')
 def quickform(context, view_name, **kwargs):
-    rev = reverse(view_name)
+    """
+        {% if request.session.pick_form.name %}
+            {% with pf=request.session.pick_form %}
+                {{ request.session.pick_form.url }}
+                {% quickform pf.name view_args=pf.name_args %}
+            {% endwith %}
+        {% endif %}
+    """
+    args = kwargs.get('view_args', ())
+    rev = reverse(view_name, args=args)
     vv = resolve(rev)
     form = vv.func.view_class(request=context.get('request')).get_form()
     form.action_url = rev
