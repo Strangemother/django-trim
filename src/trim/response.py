94db33ab4d16d16556262b2e2c53977903aaa6c6
Jay Jagpal <jay@strangemother.com>
2025-08-06 22:53:18 +0100

    Apply function comments

--- src/trim/response.py
+++ src/trim/response.py
@@ -2,6 +2,14 @@ from pathlib import Path
 from django.http import FileResponse
 
 def content_type_response(filepath, ext=None, content_types=None, default=None):
+    """Return a `FileResponse` type with the Content-Disposition applied.
+
+        from trim.response import content_type_response
+
+        real_filepath = Path('real/file.js')
+        return content_type_response(real_filepath)
+
+    """
     default = default or 'application/octet-stream'
 
     content_type_map = content_types or {
